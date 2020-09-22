0e2ea447d77f68ff0e11f814a4417dd1fdb15e15
Joshua Riley <Joshy@Gilly-in-Philly.localdomain>
2020-09-21 20:36:41 -0700

    cart fully functional

--- resources/js/script.js
+++ resources/js/script.js
@@ -8,6 +8,35 @@ const itemList = document.getElementById('item-list');
 const cartQty = document.getElementById("cart-qty");
 const cartTotal = document.getElementById("cart-total");
 
+//onchange event
+itemList.onchange = function(e) {
+    if (e.target && e.target.classList.contains('update')) {
+        const name = e.target.dataset.name;
+        const qty = parseInt(e.target.value)
+        updateCart(name, qty)
+        showItems()
+        console.log(name)
+    }
+}
+
+
+
+//onclick event
+itemList.onclick = function(e) {
+
+    if (e.target && e.target.classList.contains('remove')) {
+        const name = e.target.dataset.name
+        removeItem(name)
+    }
+    else if (e.target && e.target.classList.contains('add-one')) {
+        const name = e.target.dataset.name
+        addItem(name)
+    }
+    else if (e.target && e.target.classList.contains('subtract-one')) {
+        const name = e.target.dataset.name
+        removeItem(name, 1)
+    }
+}
 
 
 
@@ -50,12 +79,12 @@ function addItem(name, price) {
     for (let i = 0; i < cart.length; i++){
         if (cart[i].name === name) {
             cart[i].qty++;
+            showItems();
             return
         }
     }
     const item = {name: name, price: price, qty: 1}
     cart.push(item)
-    showItems();
 }
 
 // show items
@@ -69,7 +98,12 @@ function showItems() {
         itemStr += `<li>
         ${name} 
         $${price} x ${qty} =
-         $${qty * price}</li>`;
+         $${qty * price}
+        <button class="remove" data-name="${name}">Remove</button>
+        <button class="add-one" data-name="${name}"> + </button>
+        <button class="subtract-one" data-name="${name}"> - </button>
+        <input class="update" type="number" data-name="${name}">
+        </li>`
     }
 
     itemList.innerHTML = itemStr;
@@ -113,5 +147,20 @@ function removeItem(name, qty = 0) {
     }
 }
 
-// data.forEach(parseItems);
+//update cart
+function updateCart(name, qty) {
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].name === name) {
+            if (qty < 1) {
+                removeItem(name)
+                return
+            }
+        cart[i].qty = qty;
+        showItems()
+        return
+        } 
+    }
+}
+
+showItems()
 
